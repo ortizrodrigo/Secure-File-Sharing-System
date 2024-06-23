25e26411a3ab9326c40f381e7467ca478f2b8fba
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2024-06-23 16:01:05 -0500

    added basic skeletons for 8 main functions

--- client/client.go
+++ client/client.go
@@ -2,9 +2,60 @@ package client
 
 import (
 	"encoding/json"
-	"github.com/google/uuid"
-	"strings"
-	"fmt"
 	"errors"
+	"fmt"
 	"strconv"
+	"strings"
+
+	"github.com/google/uuid"
+
+	"crypto/aes"
+	"crypto/rsa"
+	"crypto/dsa"
 )
+
+type User struct {
+	Username   string
+	PrivateKey *rsa.PrivateKey
+	DSSignKey  *dsa.PrivateKey
+	RootKey    []byte
+}
+
+func InitUser(username string, password string) (userdataptr *User, err error) {
+	
+	return nil, nil
+}
+
+func GetUser(username string, password string) (userdataptr *User, err error) {
+
+	return nil, nil
+}
+
+func (userdata *User) StoreFile(filename string, content []byte) (err error) {
+
+	return nil
+}
+
+func (userdata *User) AppendToFile(filename string, content []byte) error {
+
+	return nil
+}
+
+func (userdata *User) LoadFile(filename string) (content []byte, err error) {
+
+	return nil, nil
+}
+
+func (userdata *User) CreateInvitation(filename string, recipientUsername string) (
+	invitationPtr uuid.UUID, err error) {
+
+	return uuid.Nil, nil
+}
+
+func (userdata *User) AcceptInvitation(senderUsername string, invitationPtr uuid.UUID, filename string) error {
+	return nil
+}
+
+func (userdata *User) RevokeAccess(filename string, recipientUsername string) error {
+	return nil
+}
